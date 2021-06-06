a97688b5ea996507d805175a03fdd74aaca2bfb3
Steffi Pereira <steffipereira23@gmail.com>
2021-06-06 11:44:26 +0100

    Refactor venue list for types and adding category

--- src/components/VenueList/presentation.styled.js
+++ src/components/VenueList/presentation.styled.js
@@ -2,10 +2,7 @@ import styled from 'styled-components'
 
 const Wrapper = styled.div`
   background: #f1f5f8;
-  display: flex;
   margin: 0 auto;
-  flex-wrap: wrap;
-  justify-content: space-evenly;
   padding: 2rem 0;
 `
 
@@ -15,35 +12,39 @@ const Card = styled.article`
   border-radius: 0.5rem;
   box-shadow: 0 5px 15px rgba(0, 0, 0, 0.1);
   text-align: center;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 20vw;
-  margin: .9rem;
+  width: 80vw;
+  margin: .9rem auto;
+`
+const Button = styled.button`
+  text-transform: uppercase;
+  background: #53e3a6;
+  color: #fff;
+  padding: .475rem .75rem;
+  font-size: .875rem;
+  border: 2px solid transparent;
+  cursor: pointer;
+  box-shadow: 0 1px 3px rgb(0 0 0 / 20%);
+  border-radius: .25rem;
+  text-align: center;
+`
+const Section = styled.div`
+  padding: 2rem 0;
 
-  h3 {
-    margin-bottom: 0;
-    font-weight: 600;
-    text-transform: uppercase;
-    font-size: 1.2rem;
-  }
   p {
     margin-bottom: 0;
   }
 `
-const Button = styled.button`
+
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  h3 {
+    font-weight: 600;
     text-transform: uppercase;
-    background: #53e3a6;
-    color: #fff;
-    padding: .475rem .75rem;
-    font-size: .875rem;
-    border: 2px solid transparent;
-    cursor: pointer;
-    box-shadow: 0 1px 3px rgb(0 0 0 / 20%);
-    border-radius: .25rem;
-    display: block;
-    text-align: center;
-    margin: 2.5rem auto;
+    font-size: 1.2rem;
+  }
 `
 
-export { Wrapper, Card, Button }
+export { Wrapper, Card, Button, Section, Header }
--- src/components/VenueList/presentation.tsx
+++ src/components/VenueList/presentation.tsx
@@ -1,11 +1,19 @@
 import React, { FC, useState } from 'react'
-import { FaAddressBook, FaMapMarkerAlt, FaWindowClose } from 'react-icons/fa';
-import { Wrapper, Card, Button } from './presentation.styled'
+import { FaAddressBook, FaMapMarkerAlt, FaStore } from 'react-icons/fa';
+import { Wrapper, Card, Button, Section, Header } from './presentation.styled'
 
 export interface ILocationProps {
   formattedAddress: []
   lat: number
   lng: number
+}
+export interface IVenueItemProps {
+  name: string
+  location: ILocationProps
+  categories: [{
+    id: number
+    name: string
+  }]
 }
 export interface IVenueListProps {
   venues: [
@@ -13,6 +21,10 @@ export interface IVenueListProps {
       id: number
       name: string
       location: ILocationProps
+      categories: [{
+        id: number
+        name: string
+      }]
     }
   ]
 }
@@ -21,35 +33,41 @@ export interface IVenueListProps {
 const VenueList: FC<IVenueListProps> = ({ venues }) => {
   return (
     <Wrapper>
-      {venues && venues.map(({ id, name, location}) => (
+      {venues && venues.map(({ id ,...props}) => (
         <Card key={id}>
-          <h3>{name}</h3>
-          <VenueItem {...location}/>
+          <VenueItem {...props}/>
         </Card>
       ))}
     </Wrapper>
   )
 }
 
-export const VenueItem: FC<ILocationProps> = ({ formattedAddress, lat, lng }) => {
+export const VenueItem: FC<IVenueItemProps> = ({ location, name, categories }) => {
   const [showMore, setShowMore] = useState(false)
+  const { formattedAddress, lat, lng } = location
   return (
     <>
-      {showMore ? (
-        <>
-          <p><FaWindowClose color="#53e3a6" className="close" onClick={() => setShowMore(!showMore)} /></p>
+      <Header>
+        <h3>{name}</h3>
+          <Button
+            type="button"
+            onClick={() => setShowMore(!showMore)}
+          >
+           Show {!showMore ? 'more' : 'less'}
+          </Button>
+        </Header>
+      {showMore && (
+        <Section>
           <p><FaAddressBook color="#53e3a6" style={{ marginRight: '.3rem'}} />{formattedAddress}</p>
           <p><FaMapMarkerAlt color="#53e3a6" style={{ marginRight: '.3rem' }} />{lat}, {lng}</p>
-        </>
-      )
-        :
-      <Button
-        type="button"
-        onClick={() => setShowMore(!showMore)}
-      >
-        Show More
-      </Button>
-      }
+          {categories.map(({ id, name }) => (
+              <div key={id}>
+                <p><FaStore color="#53e3a6" style={{ marginRight: '.3rem' }} />{name.toUpperCase()}</p>
+              </div>
+            )
+          )}
+        </Section>
+      )}
     </>
   )
 }
